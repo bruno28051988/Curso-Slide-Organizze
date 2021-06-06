1b54cb38a9999837ce76433f51edf15852a75826
Bruno Rodrigues Bordin <bbordin@server01.foursys.local>
2021-06-06 12:59:59 -0300

    update: subindo arquivo

---
+++ app/src/main/java/com/br/cursoslideorganizze/MainActivity.java
@@ -0,0 +1,71 @@
+package com.br.cursoslideorganizze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+
+        //Remover os botões com as setas para passar de slide
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        /*Modo de fazer um, não precisava criar layout para os slide
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descrição")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descrição2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descrição3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo4")
+                .description("Descrição4")
+                .image(R.drawable.quatro)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+         */
+
+
+        //Modo dois de fazer os slides, dessa vez tenho que criar os slide no layout
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+    }
+}
